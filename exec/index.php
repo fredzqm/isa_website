66c4bd6e1d41064794c078198932817ce13763ab
fredzqm <fredzqm@gmail.com>
2015-08-14 01:54:22 +0800

    add exec file

--- exec/index.php
+++ exec/index.php
@@ -1,29 +1,6 @@
 <?php require('../header.php')?>
 
 <div class="container">
-    <hr>
-    <div class="name_card">
-        <div class="col-sm-4 ">
-            <img class="thumbnail" src="img/fred.jpg">
-        </div>
-        <div class="col-sm-8">
-            <ul class="center list-group">
-                <li class="list-group-item"><h2>
-                    <span class="col-offset-md-3">Fred Zhang</span>
-                </h2></li>
-                <li class="list-group-item"><h3>
-                    <span>Major: CS,EE</span><span class="mid">Class: 2018</span>
-                </h3></li>
-                <li class="list-group-item"><h3>
-                    Position: Webmaster
-                </h3></li>
-                <li class="list-group-item"><h3>
-                    Email: <a href="mailto:zhangq2@rose-hulman.edu">zhangq2@rose-hulman.edu</a>
-                </h3></li>
-                <li class="list-group-item"><h3>His words: Welcome to the family of ISA!</h3></li>
-            </ul>
-        </div>
-    </div>
     
     <hr>
     <div class="name_card">
@@ -33,7 +10,56 @@
         <div class="col-sm-8">
             <ul class="center list-group">
                 <li class="list-group-item"><h2>
-                    <span class="col-offset-md-3">Fred Zhang</span>
+                    <span class="col-offset-md-3">Yukun (Jack) Gong</span>
+                </h2></li>
+                <li class="list-group-item"><h3>
+                    <span>Major: CHE </span><span class="mid">Class: 2017</span>
+                </h3></li>
+                <li class="list-group-item"><h3>
+                    Position: President:
+                </h3></li>
+                <li class="list-group-item"><h3>
+                    Email: <a href="mailto:gongy@rose-hulman.edu">gongy@rose-hulman.edu</a>
+                </h3></li>
+                <li class="list-group-item"><h3>Her words: What lies behind us, and what lies ahead of us, are tiny matters compared to what lies within us.</h3></li>
+            </ul>
+        </div>
+    </div>
+    
+    <hr>
+    <div class="name_card">
+        <div class="col-sm-4 ">
+            <img class="thumbnail" src="img/xiaoyin.jpg">
+        </div>
+        <div class="col-sm-8">
+            <ul class="center list-group">
+                <li class="list-group-item"><h2>
+                    <span class="col-offset-md-3">Xiaoyin (Catherine) Ling</span>
+                </h2></li>
+                <li class="list-group-item"><h3>
+                    <span>Major: BE </span><span class="mid">Class: 2017</span>
+                </h3></li>
+                <li class="list-group-item"><h3>
+                    Position: Vice President:
+                </h3></li>
+                <li class="list-group-item"><h3>
+                    Email: <a href="mailto:lingx@rose-hulman.edu">lingx@rose-hulman.edu</a>
+                </h3></li>
+                <li class="list-group-item"><h3>His words: Love traveling and seafood!!</h3></li>
+            </ul>
+        </div>
+    </div>
+    
+    
+    <hr>
+    <div class="name_card">
+        <div class="col-sm-4 ">
+            <img class="thumbnail" src="img/fred.jpg">
+        </div>
+        <div class="col-sm-8">
+            <ul class="center list-group">
+                <li class="list-group-item"><h2>
+                    <span class="col-offset-md-3">Qinmao（Fred） Zhang</span>
                 </h2></li>
                 <li class="list-group-item"><h3>
                     <span>Major: CS,EE</span><span class="mid">Class: 2018</span>
@@ -48,6 +74,7 @@
             </ul>
         </div>
     </div>
+
 </div>
 
 <?php require('../footer.php')?>
