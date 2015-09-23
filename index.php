369c482f7b95a35fbfd0fa6ffa913bf4549d10a7
fredzqm <fredzqm@gmail.com>
2015-09-23 09:57:11 -0400

    modification for mid-autumn

--- index.php
+++ index.php
@@ -47,10 +47,10 @@
             <!-- Jssor Slider End -->
     <div id= "recent" class="inner_box col-xs-12 col-md-6 col-lg-6">
         <hr class="visible-lg">
-        <h2><a id = 'r1' href="events/events_a.php?a=Welcome_Party">Welcome back, alumni!</a></h2>
-        <h3><a id = 'r2' href="events/">On Sept. 19th, Saturday, 10:30am-1:30pm</a></h3>
-        <h3 id = "r3"> There will be a table set up between Olin first floor and Hadley Hall.</h3>
-        <h2 id = 'r4'> Come and meet the current officers and share you post-Rose experience.</h2>
+        <h2><a id = 'r1' href="events/events_a.php?a=Welcome_Party">Have a mooncake!</a></h2>
+        <h3><a id = 'r2' href="events/">On Sept. 26th, Saturday, 7:00pm-9:00pm</a></h3>
+        <h3 id = "r3"> See you at <u>Speed beach!</u></h3>
+        <h2 id = 'r4'> Come and enjoy mooncake. How full it the moon tonight?</h2>
         <br class="visible-lg">
         <hr class="visible-lg">
     </div>
